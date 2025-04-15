2847f8d48eac63c835c8e306abc875504e327e04
syahrulramadhan00 <sahrulramadhan684@gmail.com>
2025-04-15 23:55:25 +0700

    add image revised

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/syahrulramadhan00.github.io/',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
